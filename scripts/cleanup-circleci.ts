05bdb3a76acdb9c3f22e5e6a806e5bceb15fabb6
kevinwang <kevinwang5658@gmail.com>
2026-04-21 14:45:32 -0400

    fix: switched user for chown. Added cleanup script

---
+++ scripts/cleanup-circleci.ts
@@ -0,0 +1,28 @@
+import { PluginTester, testSpawn } from '@codifycli/plugin-test';
+import path from 'node:path';
+
+const pluginPath = path.resolve('../src/index.ts');
+
+// Uninstall resources that have Codify resource definitions
+await PluginTester.uninstall(pluginPath, [
+  { type: 'pyenv' },
+  { type: 'rbenv' },
+  { type: 'uv' },
+]);
+
+// Remove pre-installed tools that don't have Codify resources
+// Go
+await testSpawn('rm -rf /usr/local/go', { requiresRoot: true });
+
+// Google Cloud SDK
+await testSpawn('rm -rf /opt/google/google-cloud-sdk', { requiresRoot: true });
+
+// Apache Maven
+await testSpawn('rm -rf /usr/local/apache-maven', { requiresRoot: true });
+
+// Gradle
+await testSpawn('rm -rf /usr/local/gradle-*', { requiresRoot: true });
+
+// Yarn (installed via ~/.yarn and ~/.config/yarn)
+await testSpawn('rm -rf ~/.yarn ~/.config/yarn');
+
