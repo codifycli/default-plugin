77f9b219770503b7adcf3284695c5131087888c4
kevinwang <kevinwang5658@gmail.com>
2026-04-22 08:57:45 -0400

    feat: keep system python3

--- scripts/cleanup-github-actions.ts
+++ scripts/cleanup-github-actions.ts
@@ -3,18 +3,18 @@ import path from 'node:path';
 
 const pluginPath = path.resolve('./src/index.ts');
 
+await testSpawn('apt-get autoremove -y ruby rpm python awscli', { requiresRoot: true });
+
 // Uninstall resources that have Codify resource definitions
 await PluginTester.uninstall(pluginPath, [
   { type: 'docker' },
   { type: 'aws-cli'}
 ]);
 
-await testSpawn('apt-get autoremove -y ruby rpm python command-not-found', { requiresRoot: true });
 await testSpawn('rustup self uninstall -y');
 
 await testSpawn('rm -rf /usr/bin/go', { requiresRoot: true })
 await testSpawn('rm -rf /usr/bin/python', { requiresRoot: true })
-await testSpawn('rm -rf /usr/bin/python3', { requiresRoot: true })
 await testSpawn('rm -rf /usr/bin/ruby', { requiresRoot: true })
 
 // await testSpawn('apt install --reinstall command-not-found', { requiresRoot: true });
