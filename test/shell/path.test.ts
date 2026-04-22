5944d31eec67c934933eb3e488cf4c1b2599f86b
kevinwang <kevinwang5658@gmail.com>
2026-04-22 14:07:47 -0400

    Add logging

--- test/shell/path.test.ts
+++ test/shell/path.test.ts
@@ -64,7 +64,12 @@ describe('Path resource integration tests', async () => {
       }
     ], {
       validateApply: async () => {
-        const { data: path } = await testSpawn('echo $PATH', { interactive: true })
+        const { data: path } = await testSpawn('echo $PATH', { interactive: true });
+
+        console.log('My path', path);
+        console.log('Temp dir 1', tempDir1);
+        console.log('Temp  dir 2', tempDir2);
+
         expect(path).to.include(tempDir1);
         expect(path).to.include(tempDir2);
       },
