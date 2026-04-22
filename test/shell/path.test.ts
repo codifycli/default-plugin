3ad9f4a451e75374bd3d66fd65d7085f0eb716b4
kevinwang <kevinwang5658@gmail.com>
2026-04-22 14:25:04 -0400

    Print out zshrc as well

--- test/shell/path.test.ts
+++ test/shell/path.test.ts
@@ -69,10 +69,10 @@ describe('Path resource integration tests', async () => {
         console.log('My path', path);
         console.log('Temp dir 1', tempDir1);
         console.log('Temp  dir 2', tempDir2);
-        console.log('Zshrc', fs.readFile(`${os.homedir()}/.zshrc`))
+        console.log('Zshrc', await fs.readFile(`${os.homedir()}/.zshrc`))
 
-        // expect(path).to.include(tempDir1);
-        // expect(path).to.include(tempDir2);
+        expect(path).to.include(tempDir1);
+        expect(path).to.include(tempDir2);
       },
       validateDestroy: async () => {
         const { data: path } = await testSpawn('echo $PATH', { interactive: true })
