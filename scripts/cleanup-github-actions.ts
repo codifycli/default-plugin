3f98f2c1a36701da99a9c51a384753b65b917736
kevinwang <kevinwang5658@gmail.com>
2026-04-22 09:01:13 -0400

    feat: move apt autoremove down

--- scripts/cleanup-github-actions.ts
+++ scripts/cleanup-github-actions.ts
@@ -3,13 +3,13 @@ import path from 'node:path';
 
 const pluginPath = path.resolve('./src/index.ts');
 
-await testSpawn('apt-get autoremove -y ruby rpm python awscli', { requiresRoot: true });
-
 // Uninstall resources that have Codify resource definitions
 await PluginTester.uninstall(pluginPath, [
   { type: 'docker' },
   { type: 'aws-cli'}
 ]);
+
+await testSpawn('apt-get autoremove -y ruby rpm python awscli', { requiresRoot: true });
 
 await testSpawn('rustup self uninstall -y');
 
