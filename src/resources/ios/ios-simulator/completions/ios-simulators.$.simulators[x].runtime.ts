31cbf486e9a304ab916234d36a40c7d487d0a80e
kevinwang <kevinwang5658@gmail.com>
2026-07-06 11:36:59 -0400

    feat: add additional simulators and accept license

--- src/resources/ios/ios-simulator/completions/ios-simulators.$.simulators[x].runtime.ts
+++ src/resources/ios/ios-simulator/completions/ios-simulators.$.simulators[x].runtime.ts
@@ -1,38 +1,43 @@
-// Known CoreSimulator runtime identifiers. Each entry corresponds to an iOS
-// (or watchOS/tvOS/visionOS) runtime that can be installed via Xcode.
 export default async function loadIosSimulatorRuntimes(): Promise<string[]> {
   return [
     // iOS
-    'com.apple.CoreSimulator.SimRuntime.iOS-16-0',
-    'com.apple.CoreSimulator.SimRuntime.iOS-16-1',
-    'com.apple.CoreSimulator.SimRuntime.iOS-16-2',
-    'com.apple.CoreSimulator.SimRuntime.iOS-16-4',
-    'com.apple.CoreSimulator.SimRuntime.iOS-17-0',
-    'com.apple.CoreSimulator.SimRuntime.iOS-17-2',
-    'com.apple.CoreSimulator.SimRuntime.iOS-17-4',
-    'com.apple.CoreSimulator.SimRuntime.iOS-17-5',
-    'com.apple.CoreSimulator.SimRuntime.iOS-18-0',
-    'com.apple.CoreSimulator.SimRuntime.iOS-18-1',
-    'com.apple.CoreSimulator.SimRuntime.iOS-18-2',
-    'com.apple.CoreSimulator.SimRuntime.iOS-18-3',
+    'com.apple.CoreSimulator.SimRuntime.iOS-26-0',
     'com.apple.CoreSimulator.SimRuntime.iOS-18-4',
+    'com.apple.CoreSimulator.SimRuntime.iOS-18-3',
+    'com.apple.CoreSimulator.SimRuntime.iOS-18-2',
+    'com.apple.CoreSimulator.SimRuntime.iOS-18-1',
+    'com.apple.CoreSimulator.SimRuntime.iOS-18-0',
+    'com.apple.CoreSimulator.SimRuntime.iOS-17-5',
+    'com.apple.CoreSimulator.SimRuntime.iOS-17-4',
+    'com.apple.CoreSimulator.SimRuntime.iOS-17-2',
+    'com.apple.CoreSimulator.SimRuntime.iOS-17-0',
+    'com.apple.CoreSimulator.SimRuntime.iOS-16-4',
+    'com.apple.CoreSimulator.SimRuntime.iOS-16-2',
+    'com.apple.CoreSimulator.SimRuntime.iOS-16-1',
+    'com.apple.CoreSimulator.SimRuntime.iOS-16-0',
 
     // watchOS
-    'com.apple.CoreSimulator.SimRuntime.watchOS-10-0',
-    'com.apple.CoreSimulator.SimRuntime.watchOS-10-4',
-    'com.apple.CoreSimulator.SimRuntime.watchOS-11-0',
+    'com.apple.CoreSimulator.SimRuntime.watchOS-26-0',
+    'com.apple.CoreSimulator.SimRuntime.watchOS-11-4',
     'com.apple.CoreSimulator.SimRuntime.watchOS-11-2',
+    'com.apple.CoreSimulator.SimRuntime.watchOS-11-0',
+    'com.apple.CoreSimulator.SimRuntime.watchOS-10-4',
+    'com.apple.CoreSimulator.SimRuntime.watchOS-10-0',
 
     // tvOS
-    'com.apple.CoreSimulator.SimRuntime.tvOS-17-0',
-    'com.apple.CoreSimulator.SimRuntime.tvOS-17-4',
-    'com.apple.CoreSimulator.SimRuntime.tvOS-18-0',
+    'com.apple.CoreSimulator.SimRuntime.tvOS-26-0',
+    'com.apple.CoreSimulator.SimRuntime.tvOS-18-4',
     'com.apple.CoreSimulator.SimRuntime.tvOS-18-2',
+    'com.apple.CoreSimulator.SimRuntime.tvOS-18-0',
+    'com.apple.CoreSimulator.SimRuntime.tvOS-17-4',
+    'com.apple.CoreSimulator.SimRuntime.tvOS-17-0',
 
     // visionOS
-    'com.apple.CoreSimulator.SimRuntime.xrOS-1-0',
-    'com.apple.CoreSimulator.SimRuntime.xrOS-1-2',
-    'com.apple.CoreSimulator.SimRuntime.xrOS-2-0',
+    'com.apple.CoreSimulator.SimRuntime.xrOS-26-0',
+    'com.apple.CoreSimulator.SimRuntime.xrOS-2-4',
     'com.apple.CoreSimulator.SimRuntime.xrOS-2-2',
+    'com.apple.CoreSimulator.SimRuntime.xrOS-2-0',
+    'com.apple.CoreSimulator.SimRuntime.xrOS-1-2',
+    'com.apple.CoreSimulator.SimRuntime.xrOS-1-0',
   ];
 }
